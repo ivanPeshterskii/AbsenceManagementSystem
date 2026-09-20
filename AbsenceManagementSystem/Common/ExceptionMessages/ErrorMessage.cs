4e07cc6c8806bd2ae61b129de85a322240b8882d
Ivan Pehsterski <peshterskiivan@gmail.com>
2026-09-20 20:38:54 +0300

    add: Teacher service

---
+++ AbsenceManagementSystem/Common/ExceptionMessages/ErrorMessage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace AbsenceManagementSystem.Common.ExceptionMessages
+{
+	public static class ErrorMessage
+	{
+		public static string TwoPupilsAreNotAllowed = "There is already a pupil with same data!";
+	}
+}
+
---
+++ AbsenceManagementSystem/Services/Contracts/ITeacherService.cs
@@ -0,0 +1,17 @@
+﻿namespace AbsenceManagementSystem.Services.Contracts
+{
+    using System;
+    using AbsenceManagementSystem.Models;
+
+    public interface ITeacherService
+	{
+        public Task<IEnumerable<Child>> GetAsyncAllPupils();
+
+        public Task GetAsyncPupilById(int pupilId);
+
+        public Task AddAsyncPupil(Child child);
+
+        public Task RemoveAsyncPupil(int pupilId);
+	}
+}
+
